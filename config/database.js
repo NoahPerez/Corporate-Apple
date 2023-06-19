41d790136d2905f4156eee041004cec8ce67aafd
Noah <noahdelaweb@gmail.com>
2023-06-19 19:54:26 +0200

    added north database info

--- config/database.js
+++ config/database.js
@@ -4,12 +4,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
+      host: env('DATABASE_HOST', 'primary.postgres--2hzqdm6jwcpn.addon.code.run'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'teza-api'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', '123456'),
-      ssl: env.bool('DATABASE_SSL', false),
+      database: env('DATABASE_NAME', '_73f05189477f'),
+      user: env('DATABASE_USERNAME', '_ccf6fc489638c235'),
+      password: env('DATABASE_PASSWORD', '_8b555cefe14ce8bb159749bc2d5f4d'),
+      ssl: env.bool('DATABASE_SSL', true),
     },
   },
 });
