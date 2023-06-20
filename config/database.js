1c7c2d76d44f283925eb39c9052c730896db4b34
Noah <noahdelaweb@gmail.com>
2023-06-20 14:49:28 +0200

    remove database info

--- config/database.js
+++ config/database.js
@@ -4,12 +4,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'primary.postgres--2hzqdm6jwcpn.addon.code.run'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'teza-api'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', '123456'),
-      ssl: env.bool('DATABASE_SSL', false),
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: env.bool('DATABASE_SSL'),
     },
   },
 });
