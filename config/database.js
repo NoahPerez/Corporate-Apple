23cf58e34443749bb4c8aa79415ca1f2815c0d9f
Noah <noahdelaweb@gmail.com>
2023-06-20 14:38:10 +0200

    updated env

--- config/database.js
+++ config/database.js
@@ -6,10 +6,17 @@ module.exports = ({ env }) => ({
     connection: {
       host: env('DATABASE_HOST', 'primary.postgres--2hzqdm6jwcpn.addon.code.run'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', '_73f05189477f'),
-      user: env('DATABASE_USERNAME', '_ccf6fc489638c235'),
-      password: env('DATABASE_PASSWORD', '_8b555cefe14ce8bb159749bc2d5f4d'),
-      ssl: env.bool('DATABASE_SSL', true),
+      database: env('DATABASE_NAME', 'teza-api'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD', '123456'),
+      ssl: env.bool('DATABASE_SSL', false),
     },
   },
 });
+
+// DATABASE-HOST=primary.postgres--2hzqdm6jwcpn.addon.code.run
+//  DATABASE-PORT=5432
+// ; DATABASE_SLL=true
+// ; DATABASE_NAME=_73f05189477f
+// ; DATABASE_USERNAME=_ccf6fc489638c235
+// ; DATABASE_PASSWORD=_8b555cefe14ce8bb159749bc2d5f4d
